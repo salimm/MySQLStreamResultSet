3cb16cfc644e656c1b90cd54f05423fbec6cec80
salimm <salimm@cs.pitt.edu>
2016-05-24 06:24:29 -0400

    added experiment 2 and updated build file

--- src/experiments/Experiment2.java
+++ src/experiments/Experiment2.java
@@ -20,7 +20,7 @@ import com.mss.sql.StreamResultSet;
  * @author Salim
  *
  */
-public class Experiment1 {
+public class Experiment2 {
 
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
@@ -41,12 +41,12 @@ public class Experiment1 {
 		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		System.out.println("Connected database successfully...");
 
-		String query = "SELECT * FROM TABLE1 as t1, TABLE2 as t2 WHERE t1.id = t2.id  ";
+		String query = "SELECT * FROM TABLE1 ";
 		int step = 10000;
 
 		List<List<Long>> stats = new ArrayList<List<Long>>();
 		stats.add(exp1(1, conn, query, step));
-		stats.add(exp2(2, conn, query, step, 10));
+		 stats.add(exp2(2, conn, query, step, 10));
 		stats.add(exp2(3, conn, query, step, 100));
 		stats.add(exp2(4, conn, query, step, 1000));
 		stats.add(exp2(5, conn, query, step, 10000));
@@ -70,7 +70,7 @@ public class Experiment1 {
 			tmp = tmp.substring(0, tmp.length() - 1) + "\n";
 			index++;
 		}
-		FileWriter fw = new FileWriter(new File("out/experiment2.csv"));
+		FileWriter fw = new FileWriter(new File("out/experiment1.csv"));
 		fw.write(tmp);
 		fw.flush();
 		fw.close();
@@ -137,7 +137,7 @@ public class Experiment1 {
 				t1 = System.currentTimeMillis();
 			}
 		}
-
+		
 		return stats;
 	}
 }
